93589f1639ad6e4eb151e5be3d23620075a5b362
Molten-Sulfur <97858539+Molten-Sulfur@users.noreply.github.com>
2022-02-05 12:28:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//*** WEB MAP DIV
+
 // Instantiate a webmap - a straightforward view of a webmap already present on ArcGIS Online.
 require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
   const webmap = new WebMap({
@@ -8,6 +10,102 @@ require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
         });
   const view = new MapView({
           map: webmap,
-          container: "viewDiv"
+          container: "webMapDiv"
         });
       });
+
+
+
+
+
+
+//** FEATURE LAYER DIV
+
+// Create a new map
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, MapView, FeatureLayer) => {
+  const map = new Map({
+    basemap: "topo-vector"
+  });
+
+  // The MapView sets the extent.
+  const view = new MapView({
+    container: "featureLayerDiv",
+    map: map,
+    zoom: 9,
+    center: [-1.7, 53.5]
+  });
+  
+  // Add a feature layer
+  const featureLayer1 = new FeatureLayer({
+  url: "https://services.arcgis.com/XSeYKQzfXnEgju9o/ArcGIS/rest/services/IMD2015_7Domains/FeatureServer/0"
+  });
+  map.add(featureLayer1);
+
+  // Add another feature layer, this one with specified symbology
+  const featureLayer2 = new FeatureLayer({
+    url: "https://services1.arcgis.com/h1C9f6qsGKmqXsVs/ArcGIS/rest/services/IBAs_UK/FeatureServer/0",
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "simple-fill",
+        color: [ 155, 155, 155, 0.4 ], // Fourth value is opacity
+        outline: { 
+          width: 1,
+          color: [ 100, 100, 100, 1 ]
+        }
+      }
+    }
+  });
+  map.add(featureLayer2);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//**STL CRIME MAP DIV
+
+// Create a new map
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], (Map, MapView, FeatureLayer) => {
+  const map = new Map({
+    basemap: "topo-vector"
+  });
+
+  // The MapView sets the extent.
+  const view = new MapView({
+    container: "viewDiv2",
+    map: map,
+    zoom: 9,
+    center: [-1.7, 53.5]
+  });
+  
+  // Add a feature layer
+  const featureLayer1 = new FeatureLayer({
+  url: "https://services.arcgis.com/XSeYKQzfXnEgju9o/ArcGIS/rest/services/IMD2015_7Domains/FeatureServer/0"
+  });
+  map.add(featureLayer1);
+
+  // Add another feature layer, this one with specified symbology
+  const featureLayer2 = new FeatureLayer({
+    url: "https://services1.arcgis.com/h1C9f6qsGKmqXsVs/ArcGIS/rest/services/IBAs_UK/FeatureServer/0",
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "simple-fill",
+        color: [ 155, 155, 155, 0.4 ], // Fourth value is opacity
+        outline: { 
+          width: 1,
+          color: [ 100, 100, 100, 1 ]
+        }
+      }
+    }
+  });
+  map.add(featureLayer2);
+});
